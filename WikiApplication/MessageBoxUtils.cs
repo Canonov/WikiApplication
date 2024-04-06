7f8a8b176d25cbeb99fc5f8d06f89876e0d9792e
CanonOverseer <agent6841@gmail.com>
2024-04-07 03:22:55 +0800

    Utility Methods for MessageBoxes

---
+++ WikiApplication/MessageBoxUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.Media;
+
+namespace WikiApplication;
+
+public static class MessageBoxUtils
+{
+	public static DialogResult ShowFatalError(string message)
+	{
+		SystemSounds.Exclamation.Play();
+		return MessageBox.Show(message, @"FATAL ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+	}
+	public static DialogResult ShowError(string message) =>
+		MessageBox.Show(message, @"An Error Occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+	public static DialogResult ShowWarning(string message) =>
+		MessageBox.Show(message, @"An Error Occured", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+	public static DialogResult PromptYesNo(string message) =>
+		MessageBox.Show(message, null, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+}
