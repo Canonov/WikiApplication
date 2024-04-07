07f9516ae8b13e369b4e079fcdc0298ac5c371f1
CanonOverseer <agent6841@gmail.com>
2024-04-07 14:21:08 +0800

    Add Colors to Feedback Label

--- WikiApplication/FeedbackLevel.cs
+++ WikiApplication/FeedbackLevel.cs
@@ -18,4 +18,12 @@ public static class FeedbackLevelExtensions
 		FeedbackLevel.Error => SystemSounds.Exclamation,
 		_ => throw new ArgumentOutOfRangeException(nameof(level), level, null)
 	};
+
+	public static Color GetColor(this FeedbackLevel level) => level switch
+	{
+		FeedbackLevel.Notice => SystemColors.ControlText,
+		FeedbackLevel.Warning => Color.Goldenrod,
+		FeedbackLevel.Error => Color.DarkRed,
+		_ => throw new ArgumentOutOfRangeException(nameof(level), level, null)
+	};
 }
