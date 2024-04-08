3422c709b5af693415065300ec7d542a77319021
Canon <canonoverseer@gmail.com>
2024-04-08 13:22:13 +0800

    Add ShowInfo Method

--- WikiApplication/MessageBoxUtils.cs
+++ WikiApplication/MessageBoxUtils.cs
@@ -12,8 +12,11 @@ public static class MessageBoxUtils
 	public static DialogResult ShowError(string message) =>
 		MessageBox.Show(message, @"An Error Occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
-	public static DialogResult ShowWarning(string message) =>
-		MessageBox.Show(message, @"An Error Occured", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+	public static DialogResult ShowWarning(string message, string title = "An Error Occured") =>
+		MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+	public static DialogResult ShowInfo(string message, string title = "Alert") =>
+		MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
 
 	public static DialogResult PromptYesNo(string message) =>
 		MessageBox.Show(message, null, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
